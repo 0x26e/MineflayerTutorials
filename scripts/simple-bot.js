7e9be8eaef32ef6098276d4cdcdfb769ce8d8532
0x26e <78525013+0x26e@users.noreply.github.com>
2022-03-09 20:57:34 -0500

    Delete simple-bot.js

--- scripts/simple-bot.js
+++
@@ -1,9 +0,0 @@
-const mineflayer = require('mineflayer');
-
-// Setup bot connection
-const bot = mineflayer.createBot({
-  host: 'localhost',
-  port: '55615',
-  username: "Hello_world",
-  version: '1.8.9'
-})
