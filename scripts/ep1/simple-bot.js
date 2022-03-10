6cf562e5727aac506c4f7d0de131fae77318d1c1
0x26e <78525013+0x26e@users.noreply.github.com>
2022-03-09 20:57:20 -0500

    Create simple-bot.js

---
+++ scripts/ep1/simple-bot.js
@@ -0,0 +1,9 @@
+const mineflayer = require('mineflayer');
+
+// Setup bot connection
+const bot = mineflayer.createBot({
+  host: 'localhost',
+  port: '12345',
+  username: "Hello_world",
+  version: '1.8.9'
+});
