c993397bf99e04d290aca3bbe8c2cee964fe8ced
0x26e <78525013+0x26e@users.noreply.github.com>
2022-03-09 21:42:28 -0500

    Delete scripts/ep1 directory

--- scripts/ep1/simple-bot.js
+++
@@ -1,9 +0,0 @@
-const mineflayer = require('mineflayer');
-
-// Setup bot connection
-const bot = mineflayer.createBot({
-  host: 'localhost',
-  port: '12345',
-  username: "Hello_world",
-  version: '1.8.9'
-});
